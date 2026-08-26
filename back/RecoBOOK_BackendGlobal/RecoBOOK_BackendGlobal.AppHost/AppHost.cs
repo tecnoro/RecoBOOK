1a37ea3170b4f215a04339d601aa95bb935b54ab
PC-c/tecnoro <saulae20045@gmail.com>
2026-08-26 19:45:11 +0200

    fix(backend): configure HTTPS profile for books API

--- back/RecoBOOK_BackendGlobal/RecoBOOK_BackendGlobal.AppHost/AppHost.cs
+++ back/RecoBOOK_BackendGlobal/RecoBOOK_BackendGlobal.AppHost/AppHost.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.RecoBOOK_API_Books>("recobook-api-books");
+builder.AddProject<Projects.RecoBOOK_API_Books>("recobook-api-books", launchProfileName: "https");
 
 builder.Build().Run();
